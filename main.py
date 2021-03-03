6c89aa19971cfccddf3905ca87dcf26959a2239a
10912218 <79964596+10912218@users.noreply.github.com>
2021-03-03 20:32:56 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from myMath import myStatistics
+num1=int(input("輸入一個數"))
+num2=int(input("輸入一個數"))
+num3=int(input("輸入一個數"))
+num4=int(input("輸入一個數"))
+num5=int(input("輸入一個數"))
+S = [num1,num2,num3,num4,num5]
+print(myStatistics.myMean(S))
